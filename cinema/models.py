36c5be098516f5e0d9f9dbfff4de1932d731816f
Denys Bokov <denys.bokov@jonasundderwolf.de>
2025-09-14 17:43:59 +0200

    feat(models): create Movie model

--- cinema/models.py
+++ cinema/models.py
@@ -3,3 +3,16 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(null=True, blank=True)
+    duration = models.DurationField(null=True, blank=True)
+
+    class Meta:
+        verbose_name_plural = "Movies"
+        verbose_name = "Movie"
+
+    def __str__(self):
+        return self.title
