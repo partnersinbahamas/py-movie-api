f876b8f7dd16f197c6eb8d710cb2cc2a7d9e3f35
Denys Bokov <denys.bokov@jonasundderwolf.de>
2025-09-14 18:12:20 +0200

    feat(view): create movie views with CRUD operations

--- app/urls.py
+++ app/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
 
+BASE_API_URL = settings.BASE_API_URL
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path(f"{BASE_API_URL}cinema/", include('cinema.urls', namespace='cinema')),
 ]
--- cinema/views.py
+++ cinema/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from rest_framework.generics import get_object_or_404
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view
 
-# Create your views here.
+from .models import Movie
+from .serializers import MovieSerializer
+
+
+@api_view(['GET', 'POST'])
+def movie_list(request):
+    if request.method == 'GET':
+        movies = Movie.objects.all()
+
+        movie_serializer = MovieSerializer(movies, many=True)
+        return Response(movie_serializer.data, status=status.HTTP_200_OK)
+    else:
+        movie_serializer = MovieSerializer(data=request.data)
+        movie_serializer.is_valid(raise_exception=True)
+        movie_serializer.save()
+
+        return Response(movie_serializer.data, status=status.HTTP_201_CREATED)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def movie_detail(request, pk):
+    movie = get_object_or_404(Movie, pk=pk)
+
+    if request.method == 'GET':
+        movie_serializer = MovieSerializer(movie)
+        return Response(movie_serializer.data, status=status.HTTP_200_OK)
+    elif request.method == 'PUT':
+        movie_serializer = MovieSerializer(movie, data=request.data)
+        movie_serializer.is_valid(raise_exception=True)
+        movie_serializer.save()
+
+        return Response(movie_serializer.data, status=status.HTTP_200_OK)
+    else:
+        movie.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
